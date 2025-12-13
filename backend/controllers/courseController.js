63191b4bb6b9588985bfaed27877ea9f51470f74
Vino <vinofernando84@gmail.com>
2025-12-13 17:00:11 +0700

    add getCourse, getUserCourse, and enroll course controller

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,5 +1,19 @@
 import * as courseService from "../services/courseService.js"
 
+export const getCourse = async(req, res, next) => {
+    try{
+        const result = await courseService.getCourse()
+        res.json(result)
+    } catch (err) { next(err) }
+}
+
+export const getUserCourses = async(req, res, next) => {
+    try{
+        const result = await courseService.getUserCourses(req.user.id)
+        res.json(result)
+    } catch (err) { next(err) }
+}
+
 export const addCourse = async (req, res, next) => {
     console.log(req.body)
     try{
@@ -8,8 +22,15 @@ export const addCourse = async (req, res, next) => {
             description: req.body.description,
             price: req.body.price,
             thumbnail: req.body.thumbnail,
-            createBy: req.user.id
+            createdBy: req.user.id
         })
+        res.json(result.rows)
+    } catch (err) { next(err) }
+}
+
+export const enrollCourse = async (req, res, next) => {
+    try{
+        const result = await courseService.enrollCourse(req.user.id, req.params.courseId)
         res.json(result.rows)
     } catch (err) { next(err) }
 }

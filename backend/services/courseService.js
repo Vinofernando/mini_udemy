453b54db5b96f6f4c94f887798994bc65789c7c1
Vino <vinofernando84@gmail.com>
2025-12-13 16:57:42 +0700

    add get course for public and user, add enrollcourse

--- backend/services/courseService.js
+++ backend/services/courseService.js
@@ -1,5 +1,21 @@
 import pool from "../config/db.js";
 
+export const getCourse = async() => {
+    const result = await pool.query(
+        `SELECT * FROM course`
+    )
+
+    return result.rows
+}
+
+export const getUserCourses = async(userId) => {
+    const result = await pool.query(
+        `SELECT * FROM course WHERE created_by = $1`
+    , [userId])
+
+    return result.rows
+}
+
 export const createCourse = async ({title, description, price, thumbnail, createdBy}) => {
     if(!title || !description || !price || !thumbnail || !createdBy) throw ({status: 400, message: "All field required"})
 
@@ -11,5 +27,24 @@ export const createCourse = async ({title, description, price, thumbnail, create
         `, [title, description, price, thumbnail, createdBy]
     )
 
-    return result.rows[0]
+    return result
+}
+
+export const enrollCourse = async (userId, courseId) => {
+    if(!courseId) throw ({status: 400, message: "Cant find course"})
+
+    const existingUser = await pool.query(
+        `SELECT * FROM enrollments WHERE user_id = $1`, [userId]
+    )
+    const course = await pool.query(
+        `SELECT * FROM enrollments WHERE course_id = $1`,[courseId]
+    )
+
+    if(course.rows.length > 0 || existingUser.rows.length > 0) throw {status: 400, message: "Course or user already enrolled"}
+
+    const result = pool.query(
+        `INSERT INTO enrollments(user_id, course_id) VALUES($1, $2) RETURNING *`,[userId, courseId]
+    )
+
+    return result
 }

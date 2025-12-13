e58300670796abb782b1ca6afab5f8630aaa23a4
Vino <vinofernando84@gmail.com>
2025-12-13 16:54:48 +0700

    add reset pw link and reset pw

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,8 +1,7 @@
 import pool from "../config/db.js";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
-import { sendResetPasswordEmail } from "./emailService.js";
-// import { sendResetPasswordEmail, sendSuccessReset} from "./emailService.js";
+import { sendResetPasswordEmail, sendSuccessResetPassword } from "./emailService.js";
 
 export const getUser = async (userId) => {
   const result = await pool.query(
@@ -26,9 +25,49 @@ export const forgotPassword = async (email) => {
             UPDATE users SET reset_token = $1, reset_expires = $2 WHERE user_email = $3
     `, [token, expires, email])
 
-    const link = `http://localhost:5500/frontend/view/resetpassword.html?token=${encodeURIComponent(token)}`;
+    const link = `http://localhost:5000/user/reset-token/${token}`;
 
     await sendResetPasswordEmail(email , link)
 
     return { message: "Link reset telah dikirim"}
+}
+
+export const getResetPasswordLink = async(token) => {
+    const reset_token = await pool.query(
+      `SELECT reset_token FROM users WHERE reset_token = $1`, [token]
+    )
+
+    if(reset_token.rows.length === 0) throw ({status: 400, message: "Token tidak valid"})
+
+    return { message: "Berhasil reset password"}
+}
+
+export const resetPassword = async({token, newPassword}) => {
+  const result = await pool.query(`
+      SELECT * FROM users WHERE reset_token = $1
+    `, [token])
+
+    if(result.rows.length === 0) throw {status: 400, message: "Token tidak valid dari reset password"}
+
+    const user = result.rows[0]
+    if( new Date(user.reset_expires) < new Date()){
+      await pool.query(
+        `UPDATE users
+        SET reset_token = NULL, reset_expires = NULL
+        WHERE reset_token = $1
+        `, [token])
+      throw { status: 400, message: 'Token expired'}
+    }
+
+    if(!newPassword) throw { status: 400, message: "Password tidak boleh kosong"}
+    const hashed = await bcrypt.hash(newPassword, 10)
+
+    await pool.query(
+      `UPDATE users
+      SET user_password = $1, reset_token = NULL, reset_expires = NULL
+      WHERE reset_token = $2
+      `, [hashed, token])
+
+    await sendSuccessResetPassword(user.user_email)
+    return { message: "Password berhasil direset."}
 }

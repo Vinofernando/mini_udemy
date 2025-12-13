8375e4004a3154b30cce1fe7a5603967a8681d2a
Vino <vinofernando84@gmail.com>
2025-12-13 17:00:58 +0700

    testing endpoint for reset pw

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { getUser, forgotPassword } from '../controllers/userController.js'
+import { getUser, forgotPassword, resetPassword, getToken } from '../controllers/userController.js'
 import authenticateToken from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.get('/', authenticateToken, getUser)
 router.post('/forgot-password', forgotPassword)
+router.get('/reset-token/:token', getToken)
+router.post('/reset-password', resetPassword)
 
 export default router

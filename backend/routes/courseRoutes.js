000343ec83e07a5c51b242fa1c3cc263d0224688
Vino <vinofernando84@gmail.com>
2025-12-13 17:01:49 +0700

    add public, user course and enroll endpoint

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"
 import authenticateToken from '../middleware/auth.js'
-import { addCourse } from "../controllers/courseController.js"
+import { addCourse, getCourse ,getUserCourses, enrollCourse } from "../controllers/courseController.js"
 
 const router = express.Router()
 
+router.get("/public", getCourse)
+router.get("/get", authenticateToken, getUserCourses)
 router.post("/create",authenticateToken ,addCourse)
+router.post("/enroll/:courseId", authenticateToken, enrollCourse)
 
 export default router

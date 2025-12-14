a6014f27109e2be87cbc8a197dac0298bd6b53a7
Vino <vinofernando84@gmail.com>
2025-12-14 18:23:36 +0700

    add role requirements to acces few endpoint

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import authenticateToken from '../middleware/auth.js'
 import { addCourse, getCourse ,getUserCourses, enrollCourse } from "../controllers/courseController.js"
+import { authorizeRole } from "../middleware/authorizeRole.js"
 
 const router = express.Router()
 
 router.get("/public", getCourse)
-router.get("/get", authenticateToken, getUserCourses)
-router.post("/create",authenticateToken ,addCourse)
-router.post("/enroll/:courseId", authenticateToken, enrollCourse)
+router.get("/get", authenticateToken, authorizeRole("admin"), getUserCourses)
+router.post("/create",authenticateToken, authorizeRole("instructor", "admin") ,addCourse)
+router.post("/enroll/:courseId", authenticateToken,authorizeRole("student") ,enrollCourse)
 
 export default router

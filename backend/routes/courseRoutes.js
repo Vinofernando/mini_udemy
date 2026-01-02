7125a5a62b916e96f1b7d2b2b91c8eb2cc5ebf73
Vino <vinofernando84@gmail.com>
2026-01-02 18:07:30 +0700

    add getLesson route

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import authenticateToken from '../middleware/auth.js'
-import { addCourse, getCourse ,getUserCourses, enrollCourse } from "../controllers/courseController.js"
+import { addCourse, getCourse ,getUserCourses, enrollCourse, getLesson } from "../controllers/courseController.js"
 import { authorizeRole } from "../middleware/authorizeRole.js"
 
 const router = express.Router()
@@ -9,5 +9,6 @@ router.get("/public", getCourse)
 router.get("/get", authenticateToken, authorizeRole("admin"), getUserCourses)
 router.post("/create",authenticateToken, authorizeRole("instructor", "admin") ,addCourse)
 router.post("/enroll/:courseId", authenticateToken,authorizeRole("student") ,enrollCourse)
+router.get("/:courseId/lessons", getLesson)
 
 export default router

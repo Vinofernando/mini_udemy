5dd9e2bd123a96263ee8425fcc17ebf2bbab61d4
Vino <vinofernando84@gmail.com>
2026-01-07 22:49:00 +0700

    add create lesson route

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import authenticateToken from '../middleware/auth.js'
-import { addCourse, getCourse ,getUserCourses, enrollCourse, getLesson, markCompleteLesson, getProgress } from "../controllers/courseController.js"
+import { addCourse, creatLesson, getCourse ,getUserCourses, enrollCourse, getLesson, markCompleteLesson, getProgress } from "../controllers/courseController.js"
 import { authorizeRole } from "../middleware/authorizeRole.js"
 import { checkEnrollment } from "../middleware/checkEnrollment.js"
 import { checkLessonAccess } from "../middleware/checkLessonAccess.js"
@@ -10,7 +10,8 @@ const router = express.Router()
 router.get("/public", getCourse)
 router.get("/my", authenticateToken, authorizeRole("instructor"), getUserCourses)
 router.get("/", authenticateToken, authorizeRole("admin"), getUserCourses)
-router.post("/create",authenticateToken, authorizeRole("instructor", "admin") ,addCourse)
+router.post("/create-course",authenticateToken, authorizeRole("instructor", "admin") ,addCourse)
+router.post("/:courseId/lessons/create-lesson", authenticateToken, authorizeRole("instructor", "admin"), creatLesson)
 router.post("/enroll/:courseId", authenticateToken,authorizeRole("student") ,enrollCourse)
 router.get("/:courseId/lessons", authenticateToken, authorizeRole("student", "instructor", "admin"), checkEnrollment ,getLesson)
 router.post("/:courseId/lessons/:lessonId/complete", authenticateToken, authorizeRole("instructor", "student", "admin"), checkEnrollment, checkLessonAccess, markCompleteLesson)

7505b3d0f6776f265f27c66cabe99f8d28b8259f
Vino <vinofernando84@gmail.com>
2026-01-12 22:01:35 +0700

    create function to publish and unpublish course and lesson for public api

--- backend/services/courseService.js
+++ backend/services/courseService.js
@@ -1,8 +1,8 @@
 import pool from "../config/db.js";
 
-export const getCourse = async() => {
+export const getPublicCourse = async() => {
     const result = await pool.query(
-        `SELECT * FROM course`
+        `SELECT * FROM course WHERE status = 'published'`
     )
 
     return result.rows
@@ -71,15 +71,33 @@ export const enrollCourse = async (userId, courseId) => {
     return result
 }
 
-export const getLesson = async(courseId) => {
-    if(!courseId) throw ({status: 400, message: "All field required"})
+export const getPublicLesson = async(courseId) => {
+    if(!courseId) throw ({status: 400, message: "Courseid required"})
 
     const result = await pool.query(
-        `SELECT * FROM lessons WHERE course_id = $1` ,[courseId]
+        `
+        SELECT l.lesson_id, l.title, l.order_number, l.is_preview
+        FROM lessons l
+        JOIN course c on c.course_id = l.course_id
+        WHERE 
+            l.course_id = $1
+            AND l.status = 'published'
+            AND c.status = 'published'
+        ORDER BY l.order_number
+        ` ,
+        [courseId]
     )
 
     return result
 }
+
+export const getUserLesson = async(courseId) => {
+    const result = await pool.query(
+        'SELECT lesson_id, title, order_number FROM lessons WHERE course_id = $1 ORDER BY order_number', [courseId]
+    )
+
+    return result
+}   
 
 export const markCompleteLesson = async({courseId, lessonId, userId}) => {
     if(!courseId) throw ({status: 400, message: "Course id required"})
@@ -162,7 +180,7 @@ export const deleteLesson = async({courseId, lessonId}) => {
 export const updateLessonOrder = async ({ courseId, lessonId, newOrder }) => {
   const client = await pool.connect()
 
-  try {
+  try { 
     await client.query("BEGIN")
 
     // 1️⃣ Ambil order saat ini
@@ -245,11 +263,11 @@ export const publishLesson = async({courseId, lessonId}) => {
     const result = await pool.query(
         `UPDATE lessons 
         SET status = 'published' 
-        WHERE course_id = $1 AND lesson_id = $2 
+        WHERE course_id = $1 AND lesson_id = $2 AND status != 'published'
         RETURNING lesson_id, status`, [courseId, lessonId]
     )
 
-    if(result.rows.length === 0) throw ({status: 404, message: "Lesson not found"})
+    if(result.rows.length === 0) throw ({status: 404, message: "Lesson not found or Lesson already published"})
 
     return result.rows[0]
 }
@@ -262,4 +280,81 @@ export const unpublishLesson = async({courseId, lessonId}) => {
     if(result.rows.length === 0) throw ({status: 404, message: "Lesson not found"})
 
     return result.rows[0]
+}
+
+export const publishCourse = async(courseId) => {
+    const allPublished = await pool.query(
+        `SELECT lesson_id FROM lessons WHERE course_id = $1 AND status != 'published' LIMIT 1`, [courseId]
+    )
+
+    if(allPublished.rows.length > 0) throw({status: 400, message:"All lesson in this course must published before publishing course"})
+
+    const result = await pool.query(
+        `UPDATE course SET status = 'published' WHERE course_id = $1 AND status != 'published' RETURNING course_id, status`, [courseId]
+    )
+
+    if(result.rows.length === 0) throw ({status: 404, message: "Course not found or already published"})
+
+    return result.rows[0]
+}
+
+export const unpublishCourse = async(courseId) => {
+    const result = await pool.query(
+        `UPDATE course SET status = 'draft' WHERE course_id = $1 RETURNING course_id, status`, [courseId]
+    )
+
+    if(result.rows.length === 0) throw ({status: 404, message: "Course not found"})
+
+    return result.rows[0]
+}
+
+export const getSingleLesson = async({lessonId, userId}) => {
+    const result = await pool.query(
+        `
+        SELECT 
+            l.lesson_id,
+            l.course_id,
+            l.title,
+            l.content,
+            l.status AS lesson_status,
+            l.is_preview,
+            l.order_number,
+            c.status AS course_status,
+            c.created_by
+        FROM lessons l 
+        JOIN course c on c.course_id = l.course_id
+        WHERE l.lesson_id = $1
+        `,
+        [lessonId]
+    )
+
+    if(result.rows.length === 0) {throw {status: 404, message: "Lesson not found"}}
+
+    const lesson = result.rows[0]
+
+    if(
+        lesson.course_status === 'published' &&
+        (lesson.lesson_status === 'published' || lesson.is_preview === true )
+    ){
+        return lesson 
+    }
+
+
+    if(!userId){
+        throw{status: 401, message: `Login required`,}
+    }
+
+    if(lesson.created_by === userId){
+        return lesson 
+    }
+
+    const enrollment = await pool.query(
+        `SELECT 1 FROM enrollments WHERE course_id = $1 AND user_id = $2`, [lesson.course_id, userId]
+    )
+
+    if(enrollment.rows.length > 0){
+        return lesson
+    }
+
+    throw{status: 403, message: "You are not allowed access this lesson"}
 }
